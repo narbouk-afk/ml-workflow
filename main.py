46a881ffcfd3e6bf165d06b28f781f44fa82e5f7
iliassabir <iliassabir0@gmail.com>
2022-11-04 20:17:47 +0100

    snake_case modif

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ def main(data_path, model_name):
     X, y = clean_data(data)
     X_train, X_test, y_train, y_test = split_data(X,y,0.8)
     nb_input = X.shape()
-    model = get_model(model_name, nb_input)
+    model = get_model_by_name(model_name, nb_input)
     kf = k_fold_cross_validation()
     precisions_train, recalls_train = model.train(X_train,y_train,kf)
-    model.getModel().save() # à voir
+    # model.getModel().save() # à voir
     y_predict = model.predict(X_test)
     precisions_test, recall_test = compute_precision_recall(y_predict, y_test)
     print(precisions_train, recalls_train, precisions_test, recall_test)
