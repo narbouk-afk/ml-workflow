b4ea79badc6e6f3560ef0186aeedd15e90fa0017
iliassabir <iliassabir0@gmail.com>
2022-11-04 19:46:31 +0100

    update main

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import sys
+from Utils import *
 
-def main(data, model):
-    X, y = import_data(data)
+def main(data_path, model_name):
+    data = import_data(data_path)
+    X, y = clean_data(data)
     X_train, X_test, y_train, y_test = split_data(X,y,0.8)
-    Model = Adapter(model())
-    Model.train(X_train,y_train)
-    y_predict = Model.predict(X_test)
+    nb_input = X.shape()
+    model = get_model(model_name, nb_input)
+    kf = k_fold_cross_validation()
+    precisions_train, recalls_train = model.train(X_train,y_train,kf)
+    model.getModel().save() # à voir
+    y_predict = model.predict(X_test)
+    precisions_test, recall_test = compute_precision_recall(y_predict, y_test)
+    print(precisions_train, recalls_train, precisions_test, recall_test)
+
 if __name__ == '__main__':
-    fileName = sys.argv[1]
-    model = sys.argv[2]
-    main(fileName, model)
+    data_path = sys.argv[1]
+    model_name = sys.argv[2]
+    main(data_path, model_name)
+
