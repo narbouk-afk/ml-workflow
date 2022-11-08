782cb859c67147fce79aca7bb9fe9cb41556ecf8
YE Xianzhi <xianzhi.ye@imt-atlantique.net>
2022-11-08 17:48:37 +0100

    clean

--- Utils.py
+++ Utils.py
@@ -6,14 +6,16 @@ from sklearn.impute import SimpleImputer
 import numpy as np
 import torch.nn as nn
 import torch
-import os, platform
+import os
+import platform
+
 
 class AbstractModel(ABC):
     def __init__(self, model):
         self.model = model
 
     @abstractmethod
-    def train(self, kf : KFold, X: np.array, y: np.array):
+    def train(self, kf: KFold, X: np.array, y: np.array):
         pass
 
     @abstractmethod
@@ -22,6 +24,7 @@ class AbstractModel(ABC):
 
     def get_model(self):
         return self.model
+
 
 class SklearnModel(AbstractModel):
     def __init__(self, model):
@@ -44,21 +47,21 @@ class SklearnModel(AbstractModel):
     def predict(self, X_valid) -> np.array:
         return self.model.predict(X_valid)
 
+
 class TorchModel(AbstractModel):
     def __init__(self, model: nn.Module, n_epochs=50, lr=0.01):
         super().__init__(model)
         self.criterion = nn.BCELoss()
-        self.optimizer = torch.optim.Adam(model.parameters(), lr = lr)
+        self.optimizer = torch.optim.Adam(model.parameters(), lr=lr)
         self.n_epochs = n_epochs
         self.__init_device()
         self.model.to(device=self.device)
         self.clear_cmd = 'cls' if platform.system() == "Windows" else 'clear'
 
-
     def __init_device(self):
         if torch.cuda.is_available():
             self.device = torch.device('cuda')
-        #elif torch.backends.mps.is_available():
+        # elif torch.backends.mps.is_available():
         #    self.device = torch.device('mps')
         else:
             self.device = torch.device('cpu')
@@ -85,7 +88,7 @@ class TorchModel(AbstractModel):
             precision, recall = compute_precision_recall(y_pred, y_valid)
             precisions.append(precision)
             recalls.append(recall)
-            fold_index+=1
+            fold_index += 1
             os.system(self.clear_cmd)
             print(f"training : {100*fold_index/kf.n_splits}%")
         return precisions, recalls
@@ -100,9 +103,11 @@ class TorchModel(AbstractModel):
                 data = data.to(device=self.device, dtype=torch.float32)
                 label = label.to(device=self.device, dtype=torch.float32)
                 self.optimizer.zero_grad()  # clear the gradients of all optimized variables
-                output = self.model(data)  # forward pass: compute predicted outputs by passing inputs to the model
+                # forward pass: compute predicted outputs by passing inputs to the model
+                output = self.model(data)
                 loss = self.criterion(output, label)  # calculate the loss
-                loss.backward()  # backward pass: compute gradient of the loss with respect to model parameters
+                # backward pass: compute gradient of the loss with respect to model parameters
+                loss.backward()
                 self.optimizer.step()  # perform a single optimization step (parameter update)
                 train_loss += loss.item() * data.size(0)  # update running training loss
         pass
@@ -110,24 +115,25 @@ class TorchModel(AbstractModel):
     def predict(self, X_test) -> np.array:
         self.model.eval()
         with torch.no_grad():
-            output = self.model(torch.from_numpy(X_test).float().to(self.device))
+            output = self.model(torch.from_numpy(
+                X_test).float().to(self.device))
         return torch.round(output).to('cpu').numpy()
 
 
 class TorchMLP(nn.Module):
-    def __init__(self, nb_input): # FUNCTION TO BE COMPLETED
-        super(TorchMLP,self).__init__()
+    def __init__(self, nb_input):  # FUNCTION TO BE COMPLETED
+        super(TorchMLP, self).__init__()
         self.fc1 = nn.Linear(nb_input, 64)
         self.dropout1 = nn.Dropout(p=0.3)
-        self.fc2 = nn.Linear(64,32)
+        self.fc2 = nn.Linear(64, 32)
         self.dropout2 = nn.Dropout(p=0.3)
-        self.fc3 = nn.Linear(32,16)
+        self.fc3 = nn.Linear(32, 16)
         self.dropout3 = nn.Dropout(p=0.2)
         self.fc4 = nn.Linear(16, 1)
         self.relu = nn.ReLU()
         self.sigm = nn.Sigmoid()
 
-    def forward(self,x): # FUNCTION TO BE COMPLETED
+    def forward(self, x):  # FUNCTION TO BE COMPLETED
         x = self.relu(self.fc1(x))
         x = self.dropout1(x)
         x = self.relu(self.fc2(x))
@@ -137,7 +143,8 @@ class TorchMLP(nn.Module):
         x = self.sigm(self.fc4(x))
         return x
 
-def import_data(name='ckd', path):  # (name=’ckd’ / ‘bad’, path: str)
+
+def import_data(path, name='ckd'):  # (name=’ckd’ / ‘bad’, path: str)
     if name == 'bad':
         data = pd.read_csv(path, sep=",", header=None)
 
@@ -172,10 +179,12 @@ def clean_data(data, mode='mean'):  # (data: pd.DF, mode = 'mean' / 'median')
 
     return X, y
 
-def split_data(X, y, test_size): # (X: np.array, y: np.array, test_size :float)
+
+def split_data(X, y, test_size):  # (X: np.array, y: np.array, test_size :float)
     return train_test_split(X, y, test_size=test_size, random_state=42)
 
-def get_model_by_name(name, nb_input):# -> AbstractModel
+
+def get_model_by_name(name, nb_input):  # -> AbstractModel
     # Match name:
     # Case “SVC”:
     #     Return SklearnModel(SVC())
@@ -183,15 +192,15 @@ def get_model_by_name(name, nb_input):# -> AbstractModel
     #     return TorchModel(MLP(nb_input))
     return
 
-def k_fold_cross_validation(n_splits = 5, random_state=34):
+
+def k_fold_cross_validation(n_splits=5, random_state=34):
     kf = KFold(n_splits=n_splits, shuffle=True, random_state=random_state)
     return kf
 
-def compute_precision_recall( y_pred, y_true):
+
+def compute_precision_recall(y_pred, y_true):
     temp_recall = y_pred[y_true == 1]
     temp_precision = y_true[y_pred == 1]
     recall = np.sum(temp_recall == 1) / temp_recall.size
     precision = np.sum(temp_precision == 1) / temp_precision.size
     return precision, recall
-
-
